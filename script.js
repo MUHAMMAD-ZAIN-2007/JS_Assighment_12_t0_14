74c0aa0237db5c022be2978a5cbe711df759147d
Muhammad Zain <m.zain80335@gmail.com>
2025-12-20 04:29:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+//Q1
+let ch = prompt("Enter a character:");
+let code = ch.charCodeAt(0);
+
+if (code >= 48 && code <= 57) {
+  alert("It is a Number");
+} else if (code >= 65 && code <= 90) {
+  alert("It is an Uppercase Letter");
+} else if (code >= 97 && code <= 122) {
+  alert("It is a Lowercase Letter");
+} else {
+  alert("Invalid Input");
+}
+
+//Q2
+
+let num1 = +prompt("Enter first number:");
+let num2 = +prompt("Enter second number:");
+
+if (num1 > num2) {
+  alert(num1 + " is larger");
+} else if (num2 > num1) {
+  alert(num2 + " is larger");
+} else {
+  alert("Both numbers are equal");
+}
+
+//Q3
+
+let num = +prompt("Enter a number:");
+
+if (num > 0) {
+  alert("Number is Positive");
+} else if (num < 0) {
+  alert("Number is Negative");
+} else {
+  alert("Number is Zero");
+}
+
+//Q4
+
+let char = prompt("Enter a single character:");
+char = char.toLowerCase();
+
+if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+  alert("true");
+} else {
+  alert("false");
+}
+
+//Q5
+
+let correctPassword = "zain123";
+let userPassword = prompt("Enter your password:");
+
+if (!userPassword) {
+  alert("Please enter your password");
+} else if (userPassword === correctPassword) {
+  alert("Correct! The password you entered matches the original password");
+} else {
+  alert("Incorrect password");
+}
+
+
+//Q6
+
+
+let greeting;
+let hour = 13;
+
+if (hour < 18) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+alert(greeting);
+
+
+//Q7
+
+let time = +prompt("Enter time in 24-hour format (e.g. 1900):");
+
+if (time >= 0 && time < 1200) {
+  alert("Good Morning");
+} else if (time >= 1200 && time < 1700) {
+  alert("Good Afternoon");
+} else if (time >= 1700 && time < 2100) {
+  alert("Good Evening");
+} else if (time >= 2100 && time <= 2359) {
+  alert("Good Night");
+} else {
+  alert("Invalid Time");
+}
